67b373cf0b76a8850845e7da38bf26a9b0b2a928
Shivani Virang <shivani_2312res617@iitp.ac.in>
2025-04-23 16:40:29 +0530

    analyzer done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # level2_genAI_app
+
+
+conda create --name llmapp python=3.11 -y
+
+conda activate llmapp
+
+pip install -r requirements.txt
+
+### how to run
+```bash
+ streamlit run app.py
+```
---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+import streamlit as st
+import pandas as pd
+from dotenv import load_dotenv, find_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+
+
+
+st.set_page_config(page_title="Sentiment Analyzer", layout="centered")
+def main():
+    # Load Groq API key from .env
+    load_dotenv(find_dotenv())
+    groq_api_key = os.getenv("GROQ_API_KEY")
+    
+
+    
+    st.title("📊Sentiment Analyzer using Gemma by Google (Groq API)")
+    text= st.text_input("write your sentence")
+
+    if st.button("Analyze Sentiment"):
+        
+        model = ChatGroq(model="gemma2-9b-it", api_key=groq_api_key)
+
+
+        st.markdown("### Sentiment Analysis Results")
+        prompt = (
+                "Analyze the sentiment of the following text and respond with "
+                "Positive, Negative, or Neutral.\n\n"
+                f"Text: {text}"
+        )
+        response = model.invoke([HumanMessage(content=prompt)])
+        sentiment = response.content.strip()
+        
+        st.write(f"**Text:** {text}")
+        st.write(f"**Sentiment:** {sentiment}")
+        st.markdown("---")
+        if sentiment == "negative":
+            st.image()
+if __name__ == "__main__":
+    main()
